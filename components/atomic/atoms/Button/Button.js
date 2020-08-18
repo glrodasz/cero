204c7acab10a3fa4c3a2d4880da7a9ae4836afbb
Guillermo Rodas <glrodasz@gmail.com>
2020-08-19 00:30:31 +0200

    Storybook setup and Button component draft

---
+++ components/atomic/atoms/Button/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ children }) => <button>{children}</button>;
+
+export default Button;
+
+// Button.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
---
+++ components/stories/atoms/Button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "../../atomic/atoms/Button";
+
+export default {
+  title: "Atoms/Buttosn",
+  component: Button,
+};
+
+export const Primary = () => <Button>Primary</Button>
