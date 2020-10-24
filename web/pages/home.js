401781fc779d1ff77da437f26cc022c5c18447dd
Guillermo Rodas <glrodasz@gmail.com>
2020-10-24 05:29:08 +0200

    Change home for FullHeightContent Layout

--- web/pages/home.js
+++ web/pages/home.js
@@ -5,47 +5,52 @@ import {
   Card,
   Icon,
   CenteredContent,
+  FullHeightContent,
 } from "@glrodasz/components";
 
 export default function Home() {
   return (
-    <CenteredContent>
-      <div style={{ display: "flex" }}>
-        <Avatar src="https://placeimg.com/200/200/people" />
-        <Spacer.Vertical size="xs" />
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <Heading size="lg">Buenos días, Cristian</Heading>
-          <Heading size="md" color="primary">
-            ¿Cómo quieres empezar?
-          </Heading>
-        </div>
-      </div>
-      <Spacer.Horizontal size="lg" />
-      <Card color="secondary" size="lg">
-        Buscar un espacio para trabajar
-      </Card>
-      <Spacer.Horizontal size="md" />
-      <Card color="primary" size="lg">
-        Iniciar una sesión de productividad
-      </Card>
-      <Spacer.Horizontal size="lg" />
-      <Card size="sm">
-        <Icon type="angleDown" hasBackground />
-        <Spacer.Vertical size="xs" />
-        <span>Maria ha hecho check-in en Factoria</span>
-      </Card>
-      <Spacer.Horizontal size="xs" />
-      <Card size="sm">
-        <Icon type="angleDown" hasBackground />
-        <Spacer.Vertical size="xs" />
-        <span>Maria ha hecho check-in en Factoria</span>
-      </Card>
-      <Spacer.Horizontal size="xs" />
-      <Card size="sm">
-        <Icon type="angleDown" hasBackground />
-        <Spacer.Vertical size="xs" />
-        <span>Maria ha hecho check-in en Factoria</span>
-      </Card>
-    </CenteredContent>
+    <FullHeightContent
+      content={
+        <>
+          <div style={{ display: "flex" }}>
+            <Avatar src="https://placeimg.com/200/200/people" />
+            <Spacer.Vertical size="xs" />
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <Heading size="lg">Buenos días, Cristian</Heading>
+              <Heading size="md" color="primary">
+                ¿Cómo quieres empezar?
+              </Heading>
+            </div>
+          </div>
+          <Spacer.Horizontal size="lg" />
+          <Card color="secondary" size="lg">
+            Buscar un espacio para trabajar
+          </Card>
+          <Spacer.Horizontal size="md" />
+          <Card color="primary" size="lg">
+            Iniciar una sesión de productividad
+          </Card>
+          <Spacer.Horizontal size="lg" />
+          <Card size="sm">
+            <Icon name="angleDown" hasBackground />
+            <Spacer.Vertical size="xs" />
+            <span>Maria ha hecho check-in en Factoria</span>
+          </Card>
+          <Spacer.Horizontal size="xs" />
+          <Card size="sm">
+            <Icon name="angleDown" hasBackground />
+            <Spacer.Vertical size="xs" />
+            <span>Maria ha hecho check-in en Factoria</span>
+          </Card>
+          <Spacer.Horizontal size="xs" />
+          <Card size="sm">
+            <Icon name="angleDown" hasBackground />
+            <Spacer.Vertical size="xs" />
+            <span>Maria ha hecho check-in en Factoria</span>
+          </Card>
+        </>
+      }
+    ></FullHeightContent>
   );
 }
