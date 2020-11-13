53b089a5521ffd2504d7e3e9f3e7ebe00615515b
Guillermo Rodas <glrodasz@gmail.com>
2020-11-13 14:07:26 +0100

    feat: enable tokens override to add dark mode

--- web/pages/planning.js
+++ web/pages/planning.js
@@ -10,6 +10,7 @@ import {
   AddButton,
   Card,
   Icon,
+  Task,
 } from "@glrodasz/components";
 
 import tasks from "../features/planning/api";
@@ -43,65 +44,71 @@ function Planning(props) {
   if (error) return `An error has ocurred ${error.message}`;
 
   return (
-    <FullHeightContent
-      content={
-        <>
-          <div style={{ display: "flex" }}>
-            <Avatar src="https://placeimg.com/200/200/people" />
-            <Spacer.Vertical size="xs" />
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-              }}
-            >
-              <Heading size="lg">Hola, Cristian</Heading>
-              <Paragraph size="lg">Conoce la metodologia RETO</Paragraph>
+    <>
+      <FullHeightContent
+        content={
+          <>
+            <div className="user-header">
+              <Avatar src="https://placeimg.com/200/200/people" />
+              <Spacer.Vertical size="sm" />
+              <div className="text">
+                <Heading size="lg">Hola, Cristian</Heading>
+                <Paragraph size="lg">Conoce la metodologia RETO</Paragraph>
+              </div>
             </div>
-          </div>
-          <Spacer.Horizontal size="lg" />
-          <Heading size="lg">
-            Ahora dime, ¿cuál es la primera tarea en la que trabajarás hoy?
-          </Heading>
-          <Spacer.Horizontal size="md" />
-          {data &&
-            data.map((task) => {
-              return (
-                <>
-                  <Card key={task.id}>
-                    <Spacer.Vertical size="sm" />
-                    <Paragraph weight="medium">{task.description}</Paragraph>
-                    <Spacer.Vertical size="sm" />
-                    <Icon
-                      name="trash"
-                      size="sm"
-                      onClick={() => deleteTask({ id: task.id })}
-                      background="inverted"
-                    />
-                  </Card>
-                  <Spacer.Horizontal size="xs" />
-                </>
-              );
-            })}
-          <Spacer.Horizontal size="md" />
-          <AddButton onAdd={(value) => addTask({ description: value })} focusHelpText="Presiona enter" blurHelpText="Clic para continuar">
-            Toca para agregar la tarea
-          </AddButton>
-        </>
-      }
-      footer={
-        <>
-          <Spacer.Horizontal size="lg" />
-          <Paragraph size="sm">
-            Basados en la matriz de Eisenhower priorizamos tus tareas evitando
-            listas de pendientes saturadas.
-          </Paragraph>
-          <Spacer.Horizontal size="sm" />
-          <Button type="primary">Empieza ahora</Button>
-        </>
-      }
-    />
+            <Spacer.Horizontal size="lg" />
+            <Heading size="lg">
+              Ahora dime, ¿cuál es la primera tarea en la que trabajarás hoy?
+            </Heading>
+            <Spacer.Horizontal size="md" />
+            {data &&
+              data.map((task) => {
+                return (
+                  <>
+                    <Task
+                      key={task.id}
+                      onDelete={() => deleteTask({ id: task.id })}
+                    >
+                      {task.description}
+                    </Task>
+                    <Spacer.Horizontal size="xs" />
+                  </>
+                );
+              })}
+            <Spacer.Horizontal size="md" />
+            <AddButton
+              onAdd={(value) => addTask({ description: value })}
+              focusHelpText="Presiona enter"
+              blurHelpText="Clic para continuar"
+            >
+              Toca para agregar la tarea
+            </AddButton>
+          </>
+        }
+        footer={
+          <>
+            <Spacer.Horizontal size="lg" />
+            <Paragraph size="sm">
+              Basados en la matriz de Eisenhower priorizamos tus tareas evitando
+              listas de pendientes saturadas.
+            </Paragraph>
+            <Spacer.Horizontal size="sm" />
+            <Button type="primary">Empieza ahora</Button>
+          </>
+        }
+      />
+      <style jsx>{`
+        .user-header {
+          display: flex;
+        }
+
+        .text {
+          flex-direction: column;
+          align-items: flex-start;
+          display: flex;
+        }
+      `}</style>
+    </>
   );
 }
 
